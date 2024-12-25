750f104d729cf1d76c4956f5268735ce24c6a22a
Krampus B. <tiobarryx@gmail.com>
2024-12-25 21:30:16 +0100

    Actualizar index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+const { makeWASocket, useSingleFileAuthState, fetchLatestBaileysVersion } = require('@whiskeysockets/baileys');
+const fs = require('fs');
+const path = require('path');
 
+const { state, saveState } = useSingleFileAuthState(path.join(__dirname, 'auth_info.json'));
+
+async function startBot() {
+  const { version, isLatest } = await fetchLatestBaileysVersion();
+  const socket = makeWASocket({
+    printQRInTerminal: true,
+    auth: state,
+    version,
+  });
+
+  socket.ev.on('qr', (qr) => {
+    console.log('Escanea el código QR:');
+    console.log(qr);
+  });
+
+  socket.ev.on('connection.update', (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === 'close') {
+      console.log('Conexión cerrada:', lastDisconnect.error);
+      startBot(); 
+    } else if (connection === 'open') {
+      console.log('Krampus conectado con exito');
+    }
+  });
+
+
+  socket.ev.on('creds.update', saveState);
+}
+
+startBot();
