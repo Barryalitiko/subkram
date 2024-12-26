d3090692885ac50d427b0f16a3f568b7a61e05b5
Krampus B. <tiobarryx@gmail.com>
2024-12-27 00:35:33 +0100

    Create onMessagesUpsert.js

---
+++ src/middlewares/onMessagesUpsert.js
@@ -0,0 +1,22 @@
+const { dynamicCommand } = require("../utils/dynamicCommand");
+const { loadCommonFunctions } = require("../utils/loadCommonFunctions");
+
+exports.onMessagesUpsert = async ({ socket, messages }) => {
+  // Verificamos si hay mensajes
+  if (!messages.length) {
+    return;
+  }
+
+  for (const webMessage of messages) {
+    // Cargamos funciones comunes necesarias para procesar el mensaje
+    const commonFunctions = loadCommonFunctions({ socket, webMessage });
+
+    // Si no se obtienen funciones comunes, pasamos al siguiente mensaje
+    if (!commonFunctions) {
+      continue;
+    }
+
+    // Procesamos el comando dinámico usando las funciones comunes
+    await dynamicCommand(commonFunctions);
+  }
+};
