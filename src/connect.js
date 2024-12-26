0f8173265c4e2336bd9099607367bb4b6b36ac75
Krampus B. <tiobarryx@gmail.com>
2024-12-26 11:32:28 +0100

    Create connect.js

---
+++ src/connect.js
@@ -0,0 +1,67 @@
+// services/connect.js
+const path = require("path");
+const { makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion, DisconnectReason, proto } = require("@whiskeysockets/baileys");
+const NodeCache = require("node-cache");
+const pino = require("pino");
+const { warningLog, infoLog, errorLog, sayLog, successLog } = require("../utils/logger");
+
+const msgRetryCounterCache = new NodeCache();
+
+const store = makeWASocket({
+  logger: pino().child({ level: "silent", stream: "store" }),
+});
+
+async function getMessage(key) {
+  if (!store) {
+    return proto.Message.fromObject({});
+  }
+
+  const msg = await store.loadMessage(key.remoteJid, key.id);
+  return msg ? msg.message : undefined;
+}
+
+async function connect() {
+  const { state, saveCreds } = await useMultiFileAuthState(
+    path.resolve(__dirname, "..", "assets", "auth", "baileys")
+  );
+
+  const { version } = await fetchLatestBaileysVersion();
+
+  const socket = makeWASocket({
+    version,
+    logger: pino({ level: "error" }),
+    printQRInTerminal: false,
+    auth: state,
+    keepAliveIntervalMs: 60 * 1000,
+    markOnlineOnConnect: true,
+    msgRetryCounterCache,
+    shouldSyncHistoryMessage: () => false,
+    getMessage,
+  });
+
+  socket.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+
+    if (connection === "close") {
+      const statusCode = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+      if (statusCode === DisconnectReason.loggedOut) {
+        errorLog("Bot desconectado!");
+      } else {
+        warningLog("Conexión cerrada o perdida.");
+      }
+
+      // Reintentar conexión
+      await connect();
+    } else if (connection === "open") {
+      successLog("Conexión exitosa");
+    } else {
+      infoLog("Procesando datos...");
+    }
+  });
+
+  socket.ev.on("creds.update", saveCreds);
+
+  return socket;
+}
+
+module.exports = connect;
