aa0aac5a2a822e05e7a8491edbac14c5de97a07c
Krampus B. <tiobarryx@gmail.com>
2024-12-27 00:28:37 +0100

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,39 +1,23 @@
 const { TIMEOUT_IN_MILLISECONDS_BY_EVENT } = require("./config");
-const { infoLog, errorLog } = require("./utils/logger");
-const { onMessagesUpsert } = require("./middlewares/onMessagesUpsert");
+const { onMessagesUpsert } = require("./middlewares/onMesssagesUpsert");
 const {
   onGroupParticipantsUpdate,
 } = require("./middlewares/onGroupParticipantsUpdate");
 
-async function load(socket) {
-  try {
-    infoLog("Cargando servicios...");
+exports.load = (socket) => {
+  socket.ev.on("messages.upsert", async ({ messages }) => {
+    setTimeout(() => {
+      onMessagesUpsert({ socket, messages });
+    }, TIMEOUT_IN_MILLISECONDS_BY_EVENT);
+  });
 
-    // Registrar eventos del socket
-    socket.ev.on("messages.upsert", async ({ messages }) => {
-      setTimeout(() => {
-        try {
-          onMessagesUpsert({ socket, messages });
-        } catch (error) {
-          console.error("Error en 'messages.upsert':", error);
-        }
-      }, TIMEOUT_IN_MILLISECONDS_BY_EVENT);
-    });
-
-    socket.ev.on("group-participants.update", async (data) => {
-      setTimeout(() => {
-        try {
-          onGroupParticipantsUpdate({ socket, groupParticipantsUpdate: data });
-        } catch (error) {
-          console.error("Error en 'group-participants.update':", error);
-        }
-      }, TIMEOUT_IN_MILLISECONDS_BY_EVENT);
-    });
-
-    infoLog("CARGANDO OPERACION MARSHALL...");
-  } catch (error) {
-    errorLog("Error al cargar servicios: ", error);
-  }
-}
-
-module.exports = { load };
+  socket.ev.on("group-participants.update", async (data) => {
+    setTimeout(() => {
+      try {
+        onGroupParticipantsUpdate({ socket, groupParticipantsUpdate: data });
+      } catch (error) {
+        console.error(error);
+      }
+    }, TIMEOUT_IN_MILLISECONDS_BY_EVENT);
+  });
+};
