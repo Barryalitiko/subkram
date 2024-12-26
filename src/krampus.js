bc7addefec709a1dbd9c41253cd9a7dc5dcea219
Krampus B. <tiobarryx@gmail.com>
2024-12-26 19:16:55 +0100

    Update krampus.js

--- src/krampus.js
+++ src/krampus.js
@@ -1,3 +1,9 @@
 const path = require("path");
 
 exports.PREFIX = "#";  
+
+exports.COMMANDS_DIR = path.join(__dirname, "comandos");
+
+exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
+
+exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
