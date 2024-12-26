f677a933364e0f43c065f2dbfd04aa1793d408ff
Krampus B. <tiobarryx@gmail.com>
2024-12-26 19:47:11 +0100

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,32 @@
-const { downloadContentFromMessage } = require("@whiskeysockets/baileys");
+const { downloadContentFromMessage } = require("baileys");
 const { PREFIX, COMMANDS_DIR, TEMP_DIR } = require("../config");
 const path = require("path");
 const fs = require("fs");
 const { writeFile } = require("fs/promises");
+const readline = require("readline");
 const axios = require("axios");
+
+exports.question = (message) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => rl.question(message, resolve));
+};
 
 exports.extractDataFromMessage = (webMessage) => {
   const textMessage = webMessage.message?.conversation;
-  const extendedTextMessage = webMessage.message?.extendedTextMessage?.text;
-  const fullMessage = textMessage || extendedTextMessage;
+  const extendedTextMessage = webMessage.message?.extendedTextMessage;
+  const extendedTextMessageText = extendedTextMessage?.text;
+  const imageTextMessage = webMessage.message?.imageMessage?.caption;
+  const videoTextMessage = webMessage.message?.videoMessage?.caption;
+
+  const fullMessage =
+    textMessage ||
+    extendedTextMessageText ||
+    imageTextMessage ||
+    videoTextMessage;
 
   if (!fullMessage) {
     return {
@@ -25,9 +43,12 @@ exports.extractDataFromMessage = (webMessage) => {
   }
 
   const isReply =
-    !!webMessage.message?.extendedTextMessage?.contextInfo?.quotedMessage;
+    !!extendedTextMessage && !!extendedTextMessage.contextInfo?.quotedMessage;
+
   const replyJid =
-    webMessage.message?.extendedTextMessage?.contextInfo?.participant || null;
+    !!extendedTextMessage && !!extendedTextMessage.contextInfo?.participant
+      ? extendedTextMessage.contextInfo.participant
+      : null;
 
   const userJid = webMessage?.key?.participant?.replace(
     /:[0-9][0-9]|:[0-9]/g,
@@ -55,6 +76,7 @@ exports.extractDataFromMessage = (webMessage) => {
 exports.splitByCharacters = (str, characters) => {
   characters = characters.map((char) => (char === "\\" ? "\\\\" : char));
   const regex = new RegExp(`[${characters.join("")}]`);
+
   return str
     .split(regex)
     .map((str) => str.trim())
@@ -67,10 +89,13 @@ exports.formatCommand = (text) => {
   );
 };
 
-exports.onlyLettersAndNumbers = (text) => text.replace(/[^a-zA-Z0-9]/g, "");
+exports.onlyLettersAndNumbers = (text) => {
+  return text.replace(/[^a-zA-Z0-9]/g, "");
+};
 
 exports.removeAccentsAndSpecialCharacters = (text) => {
   if (!text) return "";
+
   return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 };
 
@@ -89,6 +114,7 @@ exports.getContent = (webMessage, context) => {
 
 exports.download = async (webMessage, fileName, context, extension) => {
   const content = this.getContent(webMessage, context);
+
   if (!content) {
     return null;
   }
@@ -96,35 +122,97 @@ exports.download = async (webMessage, fileName, context, extension) => {
   const stream = await downloadContentFromMessage(content, context);
 
   let buffer = Buffer.from([]);
+
   for await (const chunk of stream) {
     buffer = Buffer.concat([buffer, chunk]);
   }
 
   const filePath = path.resolve(TEMP_DIR, `${fileName}.${extension}`);
+
   await writeFile(filePath, buffer);
 
   return filePath;
 };
+
+exports.findCommandImport = (commandName) => {
+  const command = this.readCommandImports();
+
+  let typeReturn = "";
+  let targetCommandReturn = null;
+
+  for (const [type, commands] of Object.entries(command)) {
+    if (!commands.length) {
+      continue;
+    }
+
+    const targetCommand = commands.find((cmd) =>
+      cmd.commands.map((cmd) => this.formatCommand(cmd)).includes(commandName)
+    );
+
+    if (targetCommand) {
+      typeReturn = type;
+      targetCommandReturn = targetCommand;
+      break;
+    }
+  }
+
+  return {
+    type: typeReturn,
+    command: targetCommandReturn,
+  };
+};
+
+exports.readCommandImports = () => {
+  const subdirectories = fs
+    .readdirSync(COMMANDS_DIR, { withFileTypes: true })
+    .filter((directory) => directory.isDirectory())
+    .map((directory) => directory.name);
+
+  const commandImports = {};
+
+  for (const subdir of subdirectories) {
+    const subdirectoryPath = path.join(COMMANDS_DIR, subdir);
+    const files = fs
+      .readdirSync(subdirectoryPath)
+      .filter(
+        (file) =>
+          !file.startsWith("_") &&
+          (file.endsWith(".js") || file.endsWith(".ts"))
+      )
+      .map((file) => require(path.join(subdirectoryPath, file)));
+
+    commandImports[subdir] = files;
+  }
+
+  return commandImports;
+};
+
+const onlyNumbers = (text) => text.replace(/[^0-9]/g, "");
+
+exports.onlyNumbers = onlyNumbers;
+
+exports.toUserJid = (number) => `${onlyNumbers(number)}@s.whatsapp.net`;
 
 exports.getBuffer = (url, options) => {
   return new Promise((resolve, reject) => {
     axios({
       method: "get",
       url,
-      headers: { DNT: 1, "Upgrade-Insecure-Request": 1, range: "bytes=0-" },
+      headers: {
+        DNT: 1,
+        "Upgrade-Insecure-Request": 1,
+        range: "bytes=0-",
+      },
       ...options,
       responseType: "arraybuffer",
       proxy: options?.proxy || false,
     })
-      .then((res) => resolve(res.data))
+      .then((res) => {
+        resolve(res.data);
+      })
       .catch(reject);
   });
 };
-
-const onlyNumbers = (text) => text.replace(/[^0-9]/g, "");
-exports.onlyNumbers = onlyNumbers;
-
-exports.toUserJid = (number) => `${onlyNumbers(number)}@s.whatsapp.net`;
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -134,8 +222,10 @@ exports.getRandomNumber = getRandomNumber;
 
 exports.getRandomName = (extension) => {
   const fileName = getRandomNumber(0, 999999);
+
   if (!extension) {
     return fileName.toString();
   }
+
   return `${fileName}.${extension}`;
 };
