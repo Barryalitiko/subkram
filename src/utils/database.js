ef2e44ee38655bdc359e0c5f0ecac5bd07221dbe
Krampus B. <tiobarryx@gmail.com>
2024-12-27 02:41:17 +0100

    Create database.js

---
+++ src/utils/database.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const fs = require("fs");
+
+const databasePath = path.resolve(__dirname, "..", "..", "database");
+
+const INACTIVE_GROUPS_FILE = "inactive-groups";
+const NOT_WELCOME_GROUPS_FILE = "not-welcome-groups";
+
+function createIfNotExists(fullPath) {
+  if (!fs.existsSync(fullPath)) {
+    fs.writeFileSync(fullPath, JSON.stringify([]));
+  }
+}
+
+function readJSON(jsonFile) {
+  const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
+
+  createIfNotExists(fullPath);
+
+  return JSON.parse(fs.readFileSync(fullPath, "utf8"));
+}
+
+function writeJSON(jsonFile, data) {
+  const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
+
+  createIfNotExists(fullPath);
+
+  fs.writeFileSync(fullPath, JSON.stringify(data));
+}
+
+exports.activateGroup = (groupId) => {
+  const filename = INACTIVE_GROUPS_FILE;
+
+  const inactiveGroups = readJSON(filename);
+
+  const index = inactiveGroups.indexOf(groupId);
+
+  if (index === -1) {
+    return;
+  }
+
+  inactiveGroups.splice(index, 1);
+
+  writeJSON(filename, inactiveGroups);
+};
+
+exports.deactivateGroup = (groupId) => {
+  const filename = INACTIVE_GROUPS_FILE;
+
+  const inactiveGroups = readJSON(filename);
+
+  if (!inactiveGroups.includes(groupId)) {
+    inactiveGroups.push(groupId);
+  }
+
+  writeJSON(filename, inactiveGroups);
+};
+
+exports.isActiveGroup = (groupId) => {
+  const filename = INACTIVE_GROUPS_FILE;
+
+  const inactiveGroups = readJSON(filename);
+
+  return !inactiveGroups.includes(groupId);
+};
+
