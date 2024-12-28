c8abaf260cd85c238479621c79cef3b494193b09
Krampus B. <tiobarryx@gmail.com>
2024-12-28 15:34:57 +0100

    Actualizar loadCommonFunctions.js

--- src/utils/loadCommonFunctions.js
+++ src/utils/loadCommonFunctions.js
@@ -71,32 +71,32 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
     }
   };
 
-  // Función para obtener información del video y la URL de descarga
   const getYouTubeDownloadUrl = async (videoUrl) => {
-    try {
-      if (!isValidYoutubeUrl(videoUrl)) {
-        throw new Error("URL no válida de YouTube");
-      }
-
-      // Obtener información del video
-      const info = await ytdl.getInfo(videoUrl);
-      console.log("Información del video:", info);
-
-      // Filtrar solo formatos de audio
-      const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-      if (!audioFormats.length) {
-        throw new Error("No se encontraron formatos de audio disponibles.");
-      }
-
-      console.log("Formato elegido:", audioFormats[0]);
-      return audioFormats[0].url;
-    } catch (error) {
-      console.error("Error al obtener la URL de descarga:", error);
-      throw new Error(
-        "Ocurrió un error al intentar obtener la URL de descarga. Verifica la URL."
-      );
+  try {
+    console.log("[MUSICA] Validando URL de YouTube:", videoUrl);
+    if (!isValidYoutubeUrl(videoUrl)) {
+      throw new Error("URL no válida de YouTube");
     }
-  };
+
+    // Obtener información del video
+    const info = await ytdl.getInfo(videoUrl);
+    console.log("[MUSICA] Información del video:", info);
+
+    // Filtrar solo formatos de audio
+    const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+    if (!audioFormats.length) {
+      throw new Error("No se encontraron formatos de audio disponibles.");
+    }
+
+    console.log("[MUSICA] Formatos de audio disponibles:", audioFormats);
+    return audioFormats[0].url;
+  } catch (error) {
+    console.error("[MUSICA] Error al obtener la URL de descarga:", error.message);
+    throw new Error(
+      "Ocurrió un error al intentar obtener la URL de descarga. Verifica la URL."
+    );
+  }
+};
 
   return {
     args,
