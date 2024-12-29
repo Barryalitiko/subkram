67aafa4c4fbbfadd1c2f00f8c974b545e5e687ac
Krampus B. <tiobarryx@gmail.com>
2024-12-29 11:51:52 +0100

    Crear autoReactions.js

---
+++ src/utils/autoReactions.js
@@ -0,0 +1,16 @@
+// utils/autoReactions.js
+
+const autoReactions = {
+  "gato": "🐱",
+  "perro": "🐶",
+  "feliz": "😊",
+  "triste": "😞",
+  "hola": "👋",
+  "gracias": "🙏",
+  "amor": "❤️",
+  "bye": "👋",
+  "sorry": "😔",
+  "ok": "👌"
+};
+
+module.exports = { autoReactions };
