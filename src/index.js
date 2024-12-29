8fef5ac5f8d12f0c076f271b8be015800446bb6c
Krampus B. <tiobarryx@gmail.com>
2024-12-29 03:39:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const { connect } = require("./connect");
 const { load } = require("./loader");
 const { infoLog, bannerLog } = require("./utils/logger");
-const { startDashboard } = require("./dashboard"); // Importar el dashboard
 
 async function start() {
   try {
@@ -11,10 +10,6 @@ async function start() {
     const socket = await connect();
 
     load(socket);
-
-    // Iniciar el dashboard
-    startDashboard(socket);
-    infoLog("Panel de control iniciado correctamente.");
   } catch (error) {
     console.log(error);
   }
