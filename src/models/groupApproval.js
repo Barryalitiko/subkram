989d9e066506adf67b27a25d9fc257dc2113ba41
Krampus B. <tiobarryx@gmail.com>
2024-12-29 14:01:51 +0100

    Delete src/models directory

--- src/models/groupApproval.js
+++
@@ -1,9 +0,0 @@
-// models/GroupApproval.js
-const mongoose = require("mongoose");
-
-const GroupApprovalSchema = new mongoose.Schema({
-  groupId: { type: String, required: true, unique: true },
-  isEnabled: { type: Boolean, default: false },
-});
-
-module.exports = mongoose.model("GroupApproval", GroupApprovalSchema);
