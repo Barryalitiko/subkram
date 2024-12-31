d946a390daa98976da3acc0c75c5afd165b2c6ba
Krampus B. <tiobarryx@gmail.com>
2024-12-31 14:14:45 +0100

    Crear video.js

---
+++ src/comandos/member/video.js
@@ -0,0 +1,40 @@
+const { PREFIX } = require("../../config");
+const { getVideoURL } = require("../../services/play-dl/video");
+const { InvalidParameterError } = require("../../errors/InvalidParameterError");
+
+module.exports = {
+  name: "play-video",
+  description: "Descargar videos",
+  commands: ["video", "v"],
+  usage: `${PREFIX}video LOFI Wilmer Roberts`,
+  handle: async ({
+    sendVideoFromURL,
+    args,
+    sendWaitReact,
+    sendSuccessReact,
+    sendErrorReply,
+  }) => {
+    if (!args.length) {
+      throw new InvalidParameterError(
+        "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝 👻 Indica el video que deseas descargar"
+      );
+    }
+
+    await sendWaitReact();
+
+    try {
+      const videoUrl = await getVideoURL(args.join(" "));
+
+      if (!videoUrl) {
+        await sendErrorReply("👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜B𝚘𝚝 👻 Video no encontrado");
+        return;
+      }
+
+      await sendSuccessReact();
+      await sendVideoFromURL(videoUrl);
+    } catch (error) {
+      console.log(error);
+      await sendErrorReply(error.message);
+    }
+  },
+};
