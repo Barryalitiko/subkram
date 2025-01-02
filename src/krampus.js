56384b8f64c9ee6c3b8ccb2371008957c2fe5ede
Krampus B. <tiobarryx@gmail.com>
2025-01-02 10:39:58 +0100

    Actualizar krampus.js

--- src/krampus.js
+++ src/krampus.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 exports.PREFIX = "#";  
 
+exports.SECONDARY_PREFIX = "$";
+
 exports.COMMANDS_DIR = path.join(__dirname, "comandos");
 
 exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
