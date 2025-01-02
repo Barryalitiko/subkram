f68689c3e4365c6a594800c33d115c58ec3a38e0
Krampus B. <tiobarryx@gmail.com>
2025-01-02 03:36:13 +0100

    Actualizar autounirse.js

--- src/comandos/member/autounirse.js
+++ src/comandos/member/autounirse.js
@@ -8,21 +8,21 @@ module.exports = {
   handle: async ({ fullArgs, sendReply, socket }) => {
     const invitationLink = fullArgs[0];
 
-    // Verificamos si el enlace fue proporcionado
+
     if (!invitationLink) {
       await sendReply(`Por favor, proporciona un enlace de invitación al grupo.`);
       return;
     }
 
     try {
-      // El enlace de invitación debe estar en el formato correcto
+
       const groupJid = invitationLink.split("https://chat.whatsapp.com/")[1];
       if (!groupJid) {
         await sendReply("El enlace proporcionado no es válido.");
         return;
       }
 
-      // Usamos la función para que el bot se una al grupo
+
       await socket.joinGroup(groupJid);
       await sendReply(`✅ El bot se ha unido correctamente al grupo con el enlace proporcionado.`);
     } catch (error) {
