58abef5723e3b9c725c3d7567dec7978d4ee155d
Krampus B. <tiobarryx@gmail.com>
2025-01-03 21:54:10 +0100

    Actualizar index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,19 +1,14 @@
-const { PREFIX, SECONDARY_PREFIX, OWNER_NUMBER } = require("../krampus");
+const { PREFIX, OWNER_NUMBER } = require("../krampus");
 const { toUserJid } = require("../utils");
 
-// Verificar si el prefijo es válido (admite varios prefijos)
-exports.verifyPrefix = (prefix) => [PREFIX, SECONDARY_PREFIX].includes(prefix);
-
-// Verificar si existe un tipo o comando
+exports.verifyPrefix = (prefix) => PREFIX === prefix;
 exports.hasTypeOrCommand = ({ type, command }) => type && command;
 
-// Detectar enlaces en un texto
 exports.isLink = (text) => {
   const regex = /(https?:\/\/(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/\S*)?)/g;
   return regex.test(text);
 };
 
-// Verificar si un usuario es administrador o propietario
 exports.isAdmin = async ({ remoteJid, userJid, socket }) => {
   const { participants, owner } = await socket.groupMetadata(remoteJid);
 
