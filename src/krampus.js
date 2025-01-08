729461f2a6d85c64646fd35fa3cfbfc573ef18c0
Krampus B. <tiobarryx@gmail.com>
2025-01-08 23:37:58 +0100

    Actualizar krampus.js

--- src/krampus.js
+++ src/krampus.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-exports.PREFIX = "#";  
+exports.PREFIX = "#";
 
 exports.COMMANDS_DIR = path.join(__dirname, "comandos");
 
@@ -9,3 +9,5 @@ exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
 exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
 
 exports.BOT_EMOJI = "༎OM༎";
+
+exports.BASE_API_URL = "http://localhost:4000"; // URL base de la API
