be696ef553332b71162550d6ca5697b4aae3030d
Krampus B. <tiobarryx@gmail.com>
2025-01-19 17:28:01 +0100

    Actualizar gif.js

--- src/comandos/member/gif.js
+++ src/comandos/member/gif.js
@@ -8,7 +8,7 @@ module.exports = {
   usage: `${PREFIX}gif`,
   handle: async ({ socket, remoteJid, sendReply }) => {
     try {
-      const gifUrl = 'https://drive.google.com/uc?id=1nKsYxXKjCOaQjYIytsVRYfNMpqZ4urvjcid=790b761154lvsktm7pyivxrld3kt9xaf7ozdn3m1n1zy4x84&ep=v1_gifs_search&rid=200.webp&ct=g';
+      const gifUrl = 'https://drive.google.com/uc?id=1nKsYxXKjCOaQjYIytsVRYfNMpqZ4urvj';
       const response = await axios.get(gifUrl, { responseType: 'arraybuffer' });
       const gifBuffer = response.data;
 
