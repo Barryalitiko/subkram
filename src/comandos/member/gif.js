2be5906fa0de653ff46f39da82002d63ad4ed23d
Krampus B. <tiobarryx@gmail.com>
2025-01-19 17:26:41 +0100

    Actualizar gif.js

--- src/comandos/member/gif.js
+++ src/comandos/member/gif.js
@@ -1,48 +1,24 @@
-const { PREFIX } = require("../../krampus");  // Para acceder al prefijo
-const axios = require('axios');  // Para realizar las peticiones HTTP
+const { PREFIX } = require("../../krampus");
+const axios = require('axios');
 
 module.exports = {
-  name: "sendgif",  // Nombre del comando
-  description: "Envía un GIF aleatorio desde un enlace",
-  commands: ["sendgif", "gift"],  // Comandos asociados
-  usage: `${PREFIX}sendgif`,  // Cómo usar el comando
-  cooldown: 180,  // 3 minutos de cooldown
-  handle: async ({ args, sendReply, sendReact, socket, remoteJid }) => {
+  name: "gif",
+  description: "Enviar un GIF",
+  commands: ["gif"],
+  usage: `${PREFIX}gif`,
+  handle: async ({ socket, remoteJid, sendReply }) => {
     try {
-      // Reaccionar con ⏳ mientras se procesa
-      await sendReact("⏳");
+      const gifUrl = 'https://drive.google.com/uc?id=1nKsYxXKjCOaQjYIytsVRYfNMpqZ4urvjcid=790b761154lvsktm7pyivxrld3kt9xaf7ozdn3m1n1zy4x84&ep=v1_gifs_search&rid=200.webp&ct=g';
+      const response = await axios.get(gifUrl, { responseType: 'arraybuffer' });
+      const gifBuffer = response.data;
 
-      // Enlaces de los GIFs para enviar
-      const gifLinks = [
-        "https://media1.tenor.com/m/YhGc7aQAI4oAAAAd/megumi-kato-kiss.gif",
-        "https://media1.tenor.com/m/R6hIA6K6yg8AAAAd/kono-subarashii-sekai-ni-shukufuku-wo-gif.gif",
-        // Puedes añadir más enlaces de GIFs aquí
-      ];
-
-      // Elegir un enlace aleatorio
-      const randomGifUrl = gifLinks[Math.floor(Math.random() * gifLinks.length)];
-
-      console.log(`[GIF] Enlace elegido: ${randomGifUrl}`);
-
-      // Descargar el GIF usando la función de downloadVideo
-      const downloadPath = await downloadVideo(randomGifUrl, 'gif');
-
-      console.log("[GIF] GIF descargado con éxito");
-
-      // Enviar el GIF descargado
       await socket.sendMessage(remoteJid, {
-        video: { url: downloadPath },  // Enviar el archivo como video
-        caption: "Aquí tienes tu GIF!",
-        mimetype: 'video/mp4',  // Mimetype de video, aunque sea un GIF, se envía como video
+        video: gifBuffer,
+        gifPlayback: true,
       });
-
-      // Confirmar que el proceso se completó con éxito
-      console.log("[GIF] GIF enviado con éxito");
-
     } catch (error) {
-      console.error("[GIF] Error al enviar el GIF:", error);
-      await sendReply("❌ Hubo un error al intentar enviar el GIF.");
-      await sendReact("❌");  // Reaccionar con error
+      console.error("Error al enviar el GIF:", error);
+      await sendReply("❌ Hubo un error al enviar el GIF");
     }
-  },
+  }
 };
