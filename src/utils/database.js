6c24649fce5b15d1a630ab9656da82ad0e56ac7d
Krampus B. <tiobarryx@gmail.com>
2025-01-20 21:07:55 +0100

    Actualizar database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,23 +3,11 @@ const fs = require("fs");
 
 const databasePath = path.resolve(__dirname, "..", "..", "database");
 
-// Crear el directorio `database` si no existe
-if (!fs.existsSync(databasePath)) {
-  fs.mkdirSync(databasePath, { recursive: true });
-}
-
 const INACTIVE_GROUPS_FILE = "inactive-groups";
 const NOT_WELCOME_GROUPS_FILE = "not-welcome-groups";
-const INACTIVE_AUTO_RESPONDER_GROUPS_FILE = "inactive-auto-responder-groups";
-const CLOSED_GROUPS_FILE = "closed-groups";  
+const ANTI_LINK_GROUPS_FILE = "anti-link-groups";
 
 function createIfNotExists(fullPath) {
-  // Aseguramos que el directorio exista antes de crear el archivo
-  const dirPath = path.dirname(fullPath);
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true });
-  }
-
   if (!fs.existsSync(fullPath)) {
     fs.writeFileSync(fullPath, JSON.stringify([]));
   }
@@ -27,173 +15,82 @@ function createIfNotExists(fullPath) {
 
 function readJSON(jsonFile) {
   const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
-
   createIfNotExists(fullPath);
-
   return JSON.parse(fs.readFileSync(fullPath, "utf8"));
 }
 
 function writeJSON(jsonFile, data) {
   const fullPath = path.resolve(databasePath, `${jsonFile}.json`);
-
   createIfNotExists(fullPath);
-
-  fs.writeFileSync(fullPath, JSON.stringify(data, null, 2));
+  fs.writeFileSync(fullPath, JSON.stringify(data));
 }
 
 exports.activateGroup = (groupId) => {
-  const filename = INACTIVE_GROUPS_FILE;
-
-  const inactiveGroups = readJSON(filename);
-
+  const inactiveGroups = readJSON(INACTIVE_GROUPS_FILE);
   const index = inactiveGroups.indexOf(groupId);
 
-  if (index === -1) {
-    return;
+  if (index !== -1) {
+    inactiveGroups.splice(index, 1);
+    writeJSON(INACTIVE_GROUPS_FILE, inactiveGroups);
   }
-
-  inactiveGroups.splice(index, 1);
-
-  writeJSON(filename, inactiveGroups);
 };
 
 exports.deactivateGroup = (groupId) => {
-  const filename = INACTIVE_GROUPS_FILE;
-
-  const inactiveGroups = readJSON(filename);
-
+  const inactiveGroups = readJSON(INACTIVE_GROUPS_FILE);
   if (!inactiveGroups.includes(groupId)) {
     inactiveGroups.push(groupId);
+    writeJSON(INACTIVE_GROUPS_FILE, inactiveGroups);
   }
-
-  writeJSON(filename, inactiveGroups);
 };
 
 exports.isActiveGroup = (groupId) => {
-  const filename = INACTIVE_GROUPS_FILE;
-
-  const inactiveGroups = readJSON(filename);
-
+  const inactiveGroups = readJSON(INACTIVE_GROUPS_FILE);
   return !inactiveGroups.includes(groupId);
 };
 
 exports.activateWelcomeGroup = (groupId) => {
-  const filename = NOT_WELCOME_GROUPS_FILE;
-
-  const notWelcomeGroups = readJSON(filename);
-
+  const notWelcomeGroups = readJSON(NOT_WELCOME_GROUPS_FILE);
   const index = notWelcomeGroups.indexOf(groupId);
 
-  if (index === -1) {
-    return;
+  if (index !== -1) {
+    notWelcomeGroups.splice(index, 1);
+    writeJSON(NOT_WELCOME_GROUPS_FILE, notWelcomeGroups);
   }
-
-  notWelcomeGroups.splice(index, 1);
-
-  writeJSON(filename, notWelcomeGroups);
 };
 
 exports.deactivateWelcomeGroup = (groupId) => {
-  const filename = NOT_WELCOME_GROUPS_FILE;
-
-  const notWelcomeGroups = readJSON(filename);
-
+  const notWelcomeGroups = readJSON(NOT_WELCOME_GROUPS_FILE);
   if (!notWelcomeGroups.includes(groupId)) {
     notWelcomeGroups.push(groupId);
+    writeJSON(NOT_WELCOME_GROUPS_FILE, notWelcomeGroups);
   }
-
-  writeJSON(filename, notWelcomeGroups);
 };
 
 exports.isActiveWelcomeGroup = (groupId) => {
-  const filename = NOT_WELCOME_GROUPS_FILE;
-
-  const notWelcomeGroups = readJSON(filename);
-
+  const notWelcomeGroups = readJSON(NOT_WELCOME_GROUPS_FILE);
   return !notWelcomeGroups.includes(groupId);
 };
 
-exports.getAutoResponderResponse = (match) => {
-  const filename = "auto-responder";
-
-  const responses = readJSON(filename);
-
-  const matchUpperCase = match.toLocaleUpperCase();
-
-  const data = responses.find(
-    (response) => response.match.toLocaleUpperCase() === matchUpperCase
-  );
-
-  if (!data) {
-    return null;
+exports.activateAntiLinkGroup = (groupId) => {
+  const antiLinkGroups = readJSON(ANTI_LINK_GROUPS_FILE);
+  if (!antiLinkGroups.includes(groupId)) {
+    antiLinkGroups.push(groupId);
+    writeJSON(ANTI_LINK_GROUPS_FILE, antiLinkGroups);
   }
-
-  return data.answer;
 };
 
-exports.activateAutoResponderGroup = (groupId) => {
-  const filename = INACTIVE_AUTO_RESPONDER_GROUPS_FILE;
+exports.deactivateAntiLinkGroup = (groupId) => {
+  const antiLinkGroups = readJSON(ANTI_LINK_GROUPS_FILE);
+  const index = antiLinkGroups.indexOf(groupId);
 
-  const inactiveAutoResponderGroups = readJSON(filename);
-
-  const index = inactiveAutoResponderGroups.indexOf(groupId);
-
-  if (index === -1) {
-    return;
-  }
-
-  inactiveAutoResponderGroups.splice(index, 1);
-
-  writeJSON(filename, inactiveAutoResponderGroups);
-};
-
-exports.deactivateAutoResponderGroup = (groupId) => {
-  const filename = INACTIVE_AUTO_RESPONDER_GROUPS_FILE;
-
-  const inactiveAutoResponderGroups = readJSON(filename);
-
-  if (!inactiveAutoResponderGroups.includes(groupId)) {
-    inactiveAutoResponderGroups.push(groupId);
-  }
-
-  writeJSON(filename, inactiveAutoResponderGroups);
-};
-
-exports.isActiveAutoResponderGroup = (groupId) => {
-  const filename = INACTIVE_AUTO_RESPONDER_GROUPS_FILE;
-
-  const inactiveAutoResponderGroups = readJSON(filename);
-
-  return !inactiveAutoResponderGroups.includes(groupId);
-};
-
-exports.closeGroup = (groupId) => {
-  const filename = CLOSED_GROUPS_FILE;
-  const closedGroups = readJSON(filename);
-  if (this.isGroupClosed(groupId)) {
-    console.log(`El grupo ${groupId} ya está cerrado`);
-    return;
-  }
-  closedGroups.push(groupId);
-  console.log(`Grupo cerrado: ${groupId}`);
-  writeJSON(filename, closedGroups);
-};
-
-exports.openGroup = (groupId) => {
-  const filename = CLOSED_GROUPS_FILE;
-  const closedGroups = readJSON(filename);
-  const index = closedGroups.indexOf(groupId);
   if (index !== -1) {
-    closedGroups.splice(index, 1);
-    console.log(`Grupo abierto: ${groupId}`);
-  } else {
-    console.log(`El grupo ${groupId} ya está abierto`);
+    antiLinkGroups.splice(index, 1);
+    writeJSON(ANTI_LINK_GROUPS_FILE, antiLinkGroups);
   }
-  writeJSON(filename, closedGroups);
 };
 
-exports.isGroupClosed = (groupId) => {
-  const filename = CLOSED_GROUPS_FILE;
-  const closedGroups = readJSON(filename);
-  return closedGroups.includes(groupId);
+exports.isActiveAntiLinkGroup = (groupId) => {
+  const antiLinkGroups = readJSON(ANTI_LINK_GROUPS_FILE);
+  return antiLinkGroups.includes(groupId);
 };
+
