aefc923e6c6e1a7e92b13ec81286cb184515bb56
Krampus B. <tiobarryx@gmail.com>
2025-01-25 22:47:24 +0100

    Actualizar instagram.js

--- src/comandos/member/instagram.js
+++ src/comandos/member/instagram.js
@@ -1,5 +1,5 @@
 const { PREFIX } = require("../../krampus");
-const { downloadInstagramVideo } = require("../../services/ytdpl"); // Ruta correcta
+const { downloadInstagramVideo } = require("../../services/ytdpl");
 const fs = require("fs");
 
 module.exports = {
@@ -7,7 +7,16 @@ module.exports = {
   description: "Descargar un video de Instagram.",
   commands: ["instagram", "insta"],
   usage: `${PREFIX}downloadinstagram <URL del video de Instagram>`,
-  handle: async ({ args, socket, remoteJid, sendReply, sendReact, webMessage }) => {
+
+  handle: async ({
+    args,
+    socket,
+    remoteJid,
+    sendReply,
+    sendReact,
+    webMessage,
+    sendMessage,
+  }) => {
     try {
       const instagramUrl = args[0];
       if (!instagramUrl) {
@@ -15,27 +24,21 @@ module.exports = {
         return;
       }
 
-      // Responder con un mensaje de "procesando..."
       await sendReply(`𝙸𝚗𝚒𝚌𝚒𝚊𝚗𝚍𝚘 𝚍𝚎𝚜𝚌𝚊𝚛𝚐𝚊...\n> Krampus OM bot`);
 
-      // Reaccionar con ⏳ al recibir el comando
       await sendReact("⏳", webMessage.key);
 
-      // Descargar el video usando la función para Instagram
       const videoPath = await downloadInstagramVideo(instagramUrl);
 
-      // Cambiar la reacción a 🎬 una vez que el video se descargó
       await sendReact("🧡", webMessage.key);
 
-      // Enviar el video descargado
-      await socket.sendMessage(remoteJid, {
-        video: { url: videoPath },
+      await sendMessage({
+        messageType: "video",
+        url: videoPath,
+        mimetype: "video/mp4",
         caption: `> Krampus OM bot\n𝚅𝚒𝚍𝚎𝚘 𝚍𝚎 𝙸𝚗𝚜𝚝𝚊𝚐𝚛𝚊𝚖 𝚌𝚊𝚛𝚐𝚊𝚍𝚘.`,
-        quoted: webMessage, // Responde al mensaje original del usuario
-        ptt: false, // Enviar como video normal, no como nota
       });
 
-      // Eliminar el archivo después de enviarlo
       setTimeout(() => {
         fs.unlink(videoPath, (err) => {
           if (err) {
@@ -44,8 +47,7 @@ module.exports = {
             console.log(`Archivo de video eliminado: ${videoPath}`);
           }
         });
-      }, 1 * 60 * 1000); // Eliminar después de 1 minuto
-
+      }, 1 * 60 * 1000);
     } catch (error) {
       console.error("Error al descargar el video de Instagram:", error);
       await sendReply("❌ Hubo un error al descargar el video de Instagram.");
