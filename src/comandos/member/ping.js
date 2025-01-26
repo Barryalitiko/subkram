d4db579d2ca83fea47a38fa147850f6d69202cb2
Krampus B. <tiobarryx@gmail.com>
2025-01-26 07:56:16 +0100

    Actualizar ping.js

--- src/comandos/member/ping.js
+++ src/comandos/member/ping.js
@@ -4,9 +4,9 @@ module.exports = {
   name: "ping",
   description: "Verificar se o bot está online",
   commands: ["ping"],
-  usage: `${PREFIX}ping`,
+  usage: `${PREFIX}om`,
   handle: async ({ sendReply, sendReact }) => {
-    await sendReact("🏓");
-    await sendReply(`🏓 Pong!`);
+    await sendReact("👻");
+    await sendReply(`Operación Marshall\n> Krampus OM bot`);
   },
 };
