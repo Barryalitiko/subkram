11f1d5f7b8cb07a8738d124ea4d6ccda40a9dbbe
Krampus B. <tiobarryx@gmail.com>
2025-01-26 07:54:54 +0100

    Actualizar perfil.js

--- src/comandos/member/perfil.js
+++ src/comandos/member/perfil.js
@@ -5,25 +5,30 @@ module.exports = {
   description: "Obtén la foto de perfil de un usuario.",
   commands: ["perfil", "pfp"],
   usage: `${PREFIX}profilepic @usuario`,
-  handle: async ({ args, socket, remoteJid, sendReply, sendReact }) => {
-    if (args.length < 1) {
-      await sendReply("Uso incorrecto. Usa el comando así:\n" + `${PREFIX}pfp @usuario`);
-      return;
-    }
+  handle: async ({ args, socket, remoteJid, sendReply, sendReact, isReply, replyJid }) => {
+    let userJid;
 
-    const userJid = args[0].replace("@", "") + "@s.whatsapp.net";
+    if (isReply) {
+      // Si el comando es una respuesta a un mensaje, obtenemos el JID del destinatario
+      userJid = replyJid;
+    } else if (args.length < 1) {
+      await sendReply("Uso incorrecto. Usa el comando así:\n" + `${PREFIX}pfp @usuario o usalo con alguien`);
+      return;
+    } else {
+      // Si el comando no es una respuesta, obtenemos el JID del argumento (usuario etiquetado)
+      userJid = args[0].replace("@", "") + "@s.whatsapp.net";
+    }
 
     try {
       // Obtener la foto de perfil del usuario
       const profilePicUrl = await socket.profilePictureUrl(userJid, "image");
 
       if (profilePicUrl) {
-        await sendReply(`> Krampus Bot👻
-          procesando...`);
+        await sendReply(`umm...\n> Krampus Bot👻`);
         await sendReact("📸");
         await socket.sendMessage(remoteJid, { image: { url: profilePicUrl }, caption: `Foto de perfil cargada...` });
       } else {
-        await sendReply(`No se pudo obtener la foto de perfil de @${args[0]}.`);
+        await sendReply(`No se pudo obtener la foto de perfil de @${args[0] || userJid.split('@')[0]}.`);
       }
     } catch (error) {
       console.error("Error al obtener la foto de perfil:", error);
