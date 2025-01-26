ce79cda70d6789dc76a468723d0c7cdb07c693a3
Krampus B. <tiobarryx@gmail.com>
2025-01-26 11:16:26 +0100

    Actualizar sticker.js

--- src/comandos/member/sticker.js
+++ src/comandos/member/sticker.js
@@ -11,10 +11,6 @@ module.exports = {
   commands: ["sticker", "s"],
   usage: `${PREFIX}sticker`,
   handle: async ({
-    isImage,
-    isVideo,
-    downloadImage,
-    downloadVideo,
     webMessage,
     sendReply,
     sendReact,
@@ -28,49 +24,37 @@ module.exports = {
         return;
       }
 
-      const isImageMessage = isImage(quoted);
-      const isVideoMessage = isVideo(quoted);
-
-      if (!isImageMessage && !isVideoMessage) {
+      if (quoted.type === 'image') {
+        await sendReact("🤔", webMessage.key);
+        const buffer = await quoted.download();
+        const sticker = await createSticker(buffer, {
+          type: "image",
+          pack: "Operacion Marshall",
+          author: "Krampus OM Bot",
+          quality: 70,
+        });
+        await sendMessage(webMessage.key.remoteJid, {
+          sticker: sticker,
+          quoted: webMessage,
+        });
+        await sendReact("🧩", webMessage.key);
+      } else if (quoted.type === 'video') {
+        await sendReact("🤔", webMessage.key);
+        const buffer = await quoted.download();
+        const sticker = await createSticker(buffer, {
+          type: "full",
+          pack: "Operacion Marshall",
+          author: "Krampus OM Bot",
+          quality: 70,
+        });
+        await sendMessage(webMessage.key.remoteJid, {
+          sticker: sticker,
+          quoted: webMessage,
+        });
+        await sendReact("🧩", webMessage.key);
+      } else {
         await sendReply("❌ Responde a una imagen o video con el comando para convertirlo en un sticker.");
-        return;
       }
-
-      await sendReact("🤔", webMessage.key);
-
-      let buffer;
-      let filePath;
-
-      if (isImageMessage) {
-        buffer = await downloadImage(quoted, "input");
-        filePath = path.join(TEMP_FOLDER, "image.png");
-        fs.writeFileSync(filePath, buffer);
-      } else if (isVideoMessage) {
-        buffer = await downloadVideo(quoted, "input");
-        filePath = path.join(TEMP_FOLDER, "video.mp4");
-        fs.writeFileSync(filePath, buffer);
-      }
-
-      if (!buffer) {
-        await sendReply("❌ No se pudo descargar el archivo. Intenta nuevamente.");
-        return;
-      }
-
-      const sticker = await createSticker(filePath, {
-        type: "full",
-        pack: "Operacion Marshall",
-        author: "Krampus OM Bot",
-        quality: 70,
-      });
-
-      await sendMessage(webMessage.key.remoteJid, {
-        sticker: sticker,
-        quoted: webMessage,
-      });
-
-      await sendReact("🧩", webMessage.key);
-
-      fs.unlinkSync(filePath);
     } catch (error) {
       console.error("Error al crear el sticker:", error);
       await sendReply("❌ Ocurrió un error al crear el sticker. Por favor, inténtalo de nuevo.");
