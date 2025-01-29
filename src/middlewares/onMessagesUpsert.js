3d6a325e4cd3fd372edaa255824a4f2e701e3a9a
Krampus B. <tiobarryx@gmail.com>
2025-01-30 00:00:36 +0100

    Actualizar onMessagesUpsert.js

--- src/middlewares/onMessagesUpsert.js
+++ src/middlewares/onMessagesUpsert.js
@@ -1,18 +1,35 @@
 const { dynamicCommand } = require("../utils/dynamicCommand");
 const { loadCommonFunctions } = require("../utils/loadCommonFunctions");
+const { autoReactions } = require("../utils/autoReactions");
 
 exports.onMessagesUpsert = async ({ socket, messages }) => {
-  if (!messages.length) {
-    return;
+if (!messages.length) {
+return;
+}
+
+for (const webMessage of messages) {
+const commonFunctions = loadCommonFunctions({ socket, webMessage });
+if (!commonFunctions) {
+continue;
+}
+
+const messageText = webMessage.message.conversation;
+const remoteJid = webMessage.key.remoteJid;
+
+// Auto Reacciones
+for (const [keyword, emoji] of Object.entries(autoReactions)) {
+  if (messageText.toLowerCase().includes(keyword)) {
+    await socket.sendMessage(remoteJid, {
+      react: {
+        text: emoji,
+        key: webMessage.key,
+      },
+    });
   }
+}
 
-  for (const webMessage of messages) {
-    const commonFunctions = loadCommonFunctions({ socket, webMessage });
-
-    if (!commonFunctions) {
-      continue;
-    }
-
-    await dynamicCommand(commonFunctions);
-  }
+// Comandos dinámicos
+await dynamicCommand(commonFunctions);
+}
 };
+
