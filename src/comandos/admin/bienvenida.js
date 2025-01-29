e707aac221df3d337364cacc040c28d7c24ce8f0
Krampus B. <tiobarryx@gmail.com>
2025-01-29 23:23:06 +0100

    Actualizar bienvenida.js

--- src/comandos/admin/bienvenida.js
+++ src/comandos/admin/bienvenida.js
@@ -14,7 +14,7 @@ usage: `${PREFIX}welcome (0/1/2)`,
 handle: async ({ args, sendReply, sendSuccessReact, remoteJid }) => {
 if (!args.length) {
 throw new InvalidParameterError(
-"👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Escribe 0, 1 o 2 para configurar la bienvenida:\n\n" +
+"\n> Krampus OM bot\nEscribe 0, 1 o 2 para configurar la bienvenida:\n\n" +
 "_0_: Desactivar\n" +
 "_1_: Activar sin foto\n" +
 "_2_: Activar con foto"
@@ -25,7 +25,7 @@ const option = args[0];
 
 if (!["0", "1", "2"].includes(option)) {
   throw new InvalidParameterError(
-    "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Opción inválida. Usa:\n\n" +
+    "\n>Krampus OM bot\nOpción inválida. Usa:\n\n" +
     "*0*: Desactivar\n" +
     "*1*: Activar sin foto\n" +
     "*2*: Activar con foto"
@@ -49,7 +49,7 @@ const context =
     : "*Activada con foto*";
 
 await sendReply(
-  `👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 La bienvenida ha sido configurada como: *${context}*`
+  `La bienvenida ha sido *${context}*\n> Krampus OM bot`
 );
 },
 };
