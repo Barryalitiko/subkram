165d6b6a5b5f7f15aa5737ab4c4ce05da0ff9f10
Krampus B. <tiobarryx@gmail.com>
2025-01-30 09:38:26 +0100

    Actualizar index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -187,11 +187,7 @@ exports.readCommandImports = () => {
   return commandImports;
 };
 
-const onlyNumbers = (text) => {
-if (typeof text === 'undefined') {
-return '';
-}
-return text.replace(/[^0-9]/g, "");
+const onlyNumbers = (text) => text.replace(/[^0-9]/g, "");
 
 exports.onlyNumbers = onlyNumbers;
 
