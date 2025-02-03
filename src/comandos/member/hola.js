d7882d6a7971af2b02ce1a85c14620675bdf8f25
Krampus B. <tiobarryx@gmail.com>
2025-02-03 02:26:14 +0100

    Actualizar hola.js

--- src/comandos/member/hola.js
+++ src/comandos/member/hola.js
@@ -1,22 +1,19 @@
 const { PREFIX } = require("../../krampus");
 
 module.exports = {
-name: "hola",
-description: "prueba",
-commands: ["hola"],
-usage: `${PREFIX}hola`,
-handle: async ({ socket, remoteJid, sendReplyWithButton }) => {
-const menuMessage = `hola`;
-const buttons = [
-{
-buttonId: "ver_canal",
-buttonText: "Ver canal",
-type: 1,
-},
-];
-await sendReplyWithButton(menuMessage, buttons);
-},
+  name: "hola",
+  description: "prueba",
+  commands: ["hola"],
+  usage: `${PREFIX}hola`,
+  handle: async ({ socket, remoteJid, sendReplyWithButton }) => {
+    const buttons = [
+      {
+        buttonId: "ver_canal",
+        buttonText: "Ver canal",
+        type: 1,
+      },
+    ];
+
+    await sendReplyWithButton("Hola", buttons);
+  },
 };
-
-
-
