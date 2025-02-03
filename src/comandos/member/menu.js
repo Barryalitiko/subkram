15f78ed1f3cdf5d6d5b44d51438e870a871ee99c
Krampus B. <tiobarryx@gmail.com>
2025-02-03 23:23:22 +0100

    Update menu.js

--- src/comandos/member/menu.js
+++ src/comandos/member/menu.js
@@ -1,23 +1,18 @@
 const { PREFIX } = require("../../krampus");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   name: "menu",
   description: "Muestra el menú de comandos.",
   commands: ["menu", "help", "ayuda"],
   usage: `${PREFIX}menu`,
-  
   handle: async ({ socket, remoteJid, sendReply }) => {
     const menuMessage = `»»————- - ————-««
 > 𝗞𝗿𝗮𝗺𝗽𝘂𝘀 𝗢𝗠 𝗯𝗼𝘁
-
-═════════.K.═
-
-COMANDOS:
-
-═.K.═════════
-
+usa ${PREFIX}menu2 para ver mas detalles
+══════════.K.═ COMANDOS: ═.K.═════════
 𝗔𝗗𝗠𝗜𝗡𝗦
-
 ⌠⅏⌡➟ ${PREFIX}cerrar
 ⌠⅏⌡➟ ${PREFIX}abrir
 ⌠⅏⌡➟ ${PREFIX}antilink 0-1-2
@@ -30,12 +25,12 @@ COMANDOS:
 ⌠⅏⌡➟ ${PREFIX}tag
 ⌠⅏⌡➟ ${PREFIX}todos
 ⌠⅏⌡➟ ${PREFIX}reglas
-
-═════════.K.═
-
+⌠⅏⌡➟ ${PREFIX}ban
+══════════.K.═
 𝗠𝗜𝗘𝗠𝗕𝗥𝗢𝗦
-
 ⌠⅏⌡➟ ${PREFIX}link
+⌠⅏⌡➟ ${PREFIX}menu
+⌠⅏⌡➟ ${PREFIX}menu2
 ⌠⅏⌡➟ ${PREFIX}reglas
 ⌠⅏⌡➟ ${PREFIX}musica/m + nombre de la canción
 ⌠⅏⌡➟ ${PREFIX}video/v + nombre del video
@@ -44,20 +39,23 @@ COMANDOS:
 ⌠⅏⌡➟ ${PREFIX}ping
 ⌠⅏⌡➟ ${PREFIX}pfp/perfil
 ⌠⅏⌡➟ ${PREFIX}fotogrupo
-
 ═.K.═════════
-
 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦 𝗦𝗫
-
 ⌠⅏⌡➟ ${PREFIX}tijera
 ⌠⅏⌡➟ ${PREFIX}beso
 ⌠⅏⌡➟ ${PREFIX}penetrar
 ⌠⅏⌡➟ ${PREFIX}tocar
-
+⌠⅏⌡➟ ${PREFIX}haiti
+⌠⅏⌡➟ ${PREFIX}saborear
 »»————- - ————-««
-Operacion Marshall
+> Operacion Marshall
 »»————- - ————-««`;
 
-    await sendReply(menuMessage);
+    await socket.sendMessage(remoteJid, {
+      video: fs.readFileSync("assets/sx/menu.mp4"),
+      caption: menuMessage,
+      gifPlayback: true,
+    });
   },
 };
+
