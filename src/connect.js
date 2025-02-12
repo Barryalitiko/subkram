b65dbd7ef3a2537568056b08af3c160c2411b9b6
Krampus B. <tiobarryx@gmail.com>
2025-02-12 05:51:23 +0100

    Actualizar connect.js

--- src/connect.js
+++ src/connect.js
@@ -1,55 +1,92 @@
 const path = require("path");
 const { question, onlyNumbers } = require("./utils");
-const { default: makeWASocket, DisconnectReason, useMultiFileAuthState, fetchLatestBaileysVersion, isJidBroadcast, isJidStatusBroadcast, proto, makeInMemoryStore, isJidNewsletter } = require("@whiskeysockets/baileys");
+const {
+  default: makeWASocket,
+  DisconnectReason,
+  useMultiFileAuthState,
+  fetchLatestBaileysVersion,
+  isJidBroadcast,
+  isJidStatusBroadcast,
+  proto,
+  makeInMemoryStore,
+  isJidNewsletter,
+} = require("@whiskeysockets/baileys");
 const NodeCache = require("node-cache");
 const pino = require("pino");
 const { load } = require("./loader");
-const { warningLog, infoLog, errorLog, sayLog, successLog } = require("./utils/logger");
+const {
+  warningLog,
+  infoLog,
+  errorLog,
+  sayLog,
+  successLog,
+} = require("./utils/logger");
+
 const msgRetryCounterCache = new NodeCache();
-const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }) });
+
+const store = makeInMemoryStore({
+  logger: pino().child({ level: "silent", stream: "store" }),
+});
 
 async function getMessage(key) {
   if (!store) {
     return proto.Message.fromObject({});
   }
+
   const msg = await store.loadMessage(key.remoteJid, key.id);
+
   return msg ? msg.message : undefined;
 }
 
-async function connect(subbotNombre = null) {
-  const { state, saveCreds } = await useMultiFileAuthState(path.resolve(__dirname, "..", "assets", "auth", "baileys"));
+async function connect() {
+  const { state, saveCreds } = await useMultiFileAuthState(
+    path.resolve(__dirname, "..", "assets", "auth", "baileys")
+  );
+
   const { version } = await fetchLatestBaileysVersion();
+
   const socket = makeWASocket({
     version,
     logger: pino({ level: "error" }),
     printQRInTerminal: false,
     defaultQueryTimeoutMs: 60 * 1000,
     auth: state,
-    shouldIgnoreJid: (jid) => isJidBroadcast(jid) || isJidStatusBroadcast(jid) || isJidNewsletter(jid),
+    shouldIgnoreJid: (jid) =>
+      isJidBroadcast(jid) || isJidStatusBroadcast(jid) || isJidNewsletter(jid),
     keepAliveIntervalMs: 60 * 1000,
     markOnlineOnConnect: true,
     msgRetryCounterCache,
     shouldSyncHistoryMessage: () => false,
     getMessage,
-    subbot: subbotNombre ? true : false
   });
 
   if (!socket.authState.creds.registered) {
     warningLog("Credenciales no configuradas!");
+
     infoLog('Ingrese su numero sin el + (ejemplo: "13733665556"):');
+
     const phoneNumber = await question("Ingresa el numero: ");
+
     if (!phoneNumber) {
-      errorLog('Numero de telefono inválido! Reinicia con el comando "npm start".');
+      errorLog(
+        'Numero de telefono inválido! Reinicia con el comando "npm start".'
+      );
+
       process.exit(1);
     }
+
     const code = await socket.requestPairingCode(onlyNumbers(phoneNumber));
+
     sayLog(`Código de Emparejamiento: ${code}`);
   }
 
   socket.ev.on("connection.update", async (update) => {
     const { connection, lastDisconnect } = update;
+
     if (connection === "close") {
-      const statusCode = lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+      const statusCode =
+        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
+
       if (statusCode === DisconnectReason.loggedOut) {
         errorLog("Kram desconectado!");
       } else {
@@ -79,7 +116,8 @@ async function connect(subbotNombre = null) {
             warningLog("Servicio no disponible!");
             break;
         }
-        const newSocket = await connect(subbotNombre);
+
+        const newSocket = await connect();
         load(newSocket);
       }
     } else if (connection === "open") {
