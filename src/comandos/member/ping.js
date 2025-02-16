4ea65dfe9e04a81f3e895e18d8ffa402f62e5198
Krampus B. <tiobarryx@gmail.com>
2025-02-16 12:38:00 +0100

    Actualizar ping.js

--- src/comandos/member/ping.js
+++ src/comandos/member/ping.js
@@ -6,7 +6,24 @@ module.exports = {
   commands: ["ping"],
   usage: `${PREFIX}om`,
   handle: async ({ sendReply, sendReact }) => {
+    const link = "https://chat.whatsapp.com/F7qZTWPDTNqGALF0d9VQJC";
+    const title = "Únete al grupo de WhatsApp";
+    const description = "Haz clic para unirte al grupo";
+    const thumbnail = "https://images.vexels.com/media/users/3/215556/isolated/preview/56cab15c9dd85351a1546bdfa86b9dd6-gato-acostado-mullido-plano.png"; // reemplaza con una imagen de thumbnail
+
     await sendReact("👻");
-    await sendReply(`Operación Marshall\n> Krampus OM bot`);
-  },
+    await sendReply({
+      text: `Operación Marshall\n> Krampus OM bot`,
+      link: {
+        title,
+        description,
+        thumbnail,
+        url: link
+      }
+    });
+  }
 };
+
+
+
+
