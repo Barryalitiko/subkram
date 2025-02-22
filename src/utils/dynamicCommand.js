b581fa27f5b952167b73eee4e62f6089342ffb65
Krampus B. <tiobarryx@gmail.com>
2025-02-22 23:29:55 +0100

    Actualizar dynamicCommand.js

--- src/utils/dynamicCommand.js
+++ src/utils/dynamicCommand.js
@@ -3,89 +3,115 @@ const { InvalidParameterError } = require("../errors/InvalidParameterError");
 const { WarningError } = require("../errors/WarningError");
 const { findCommandImport } = require(".");
 const {
-verifyPrefix,
-hasTypeOrCommand,
-isLink,
-isAdmin,
+  verifyPrefix,
+  hasTypeOrCommand,
+  isLink,
+  isAdmin,
 } = require("../middlewares");
 const { checkPermission } = require("../middlewares/checkPermission");
-const { isActiveAntiLinkGroup, getAntiLinkMode } = require("./database");
+const {
+  isActiveAntiLinkGroup,
+  getAntiLinkMode,
+  isUserMuted,
+  getMuteExpiration,
+} = require("./database");
 const { errorLog } = require("../utils/logger");
 const { ONLY_GROUP_ID } = require("../krampus");
 
 function isGroupLink(message) {
-return message.includes("chat.whatsapp.com");
+  return message.includes("chat.whatsapp.com");
 }
 
 exports.dynamicCommand = async (paramsHandler) => {
-const {
-commandName,
-prefix,
-sendWarningReply,
-sendErrorReply,
-remoteJid,
-sendReply,
-socket,
-userJid,
-fullMessage,
-webMessage,
-} = paramsHandler;
+  const {
+    commandName,
+    prefix,
+    sendWarningReply,
+    sendErrorReply,
+    remoteJid,
+    sendReply,
+    socket,
+    userJid,
+    fullMessage,
+    webMessage,
+  } = paramsHandler;
 
-if (isActiveAntiLinkGroup(remoteJid) && isLink(fullMessage)) {
-const antiLinkMode = getAntiLinkMode(remoteJid);
-if (
-antiLinkMode === "2" ||
-(antiLinkMode === "1" && isGroupLink(fullMessage))
-) {
-if (!(await isAdmin({ remoteJid, userJid, socket }))) {
-await socket.groupParticipantsUpdate(remoteJid, [userJid], "remove");
-await sendReply(
-"👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Baneado por enviar link"
-);
-await socket.sendMessage(remoteJid, {
-delete: {
-remoteJid,
-fromMe: false,
-id: webMessage.key.id,
-participant: webMessage.key.participant,
-},
-});
-return;
-}
-}
-}
+  // Verificación si el usuario está muteado
+  if (await isUserMuted(remoteJid, userJid)) {
+    const muteExpiration = await getMuteExpiration(remoteJid, userJid);
+    const timeLeft = muteExpiration - Date.now();
+    if (timeLeft > 0) {
+      await sendReply(`🛑 Estás muteado. Te quedan ${Math.ceil(timeLeft / 60000)} minutos.`);
+      await socket.sendMessage(remoteJid, {
+        delete: {
+          remoteJid,
+          fromMe: false,
+          id: webMessage.key.id,
+          participant: webMessage.key.participant,
+        },
+      });
+      return;
+    } else {
+      // El muteo ha expirado, se elimina el muteo del usuario
+      await unmuteUser(remoteJid, userJid);
+    }
+  }
 
-const { type, command } = findCommandImport(commandName);
+  if (isActiveAntiLinkGroup(remoteJid) && isLink(fullMessage)) {
+    const antiLinkMode = getAntiLinkMode(remoteJid);
+    if (
+      antiLinkMode === "2" ||
+      (antiLinkMode === "1" && isGroupLink(fullMessage))
+    ) {
+      if (!(await isAdmin({ remoteJid, userJid, socket }))) {
+        await socket.groupParticipantsUpdate(remoteJid, [userJid], "remove");
+        await sendReply(
+          "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Baneado por enviar link"
+        );
+        await socket.sendMessage(remoteJid, {
+          delete: {
+            remoteJid,
+            fromMe: false,
+            id: webMessage.key.id,
+            participant: webMessage.key.participant,
+          },
+        });
+        return;
+      }
+    }
+  }
 
-if (ONLY_GROUP_ID && ONLY_GROUP_ID !== remoteJid) {
-return;
-}
+  const { type, command } = findCommandImport(commandName);
 
-if (!verifyPrefix(prefix) || !hasTypeOrCommand({ type, command })) {
-return;
-}
+  if (ONLY_GROUP_ID && ONLY_GROUP_ID !== remoteJid) {
+    return;
+  }
 
-if (!(await checkPermission({ type, ...paramsHandler }))) {
-await sendErrorReply(
-"👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 No tienes permitido usar el comando"
-);
-return;
-}
+  if (!verifyPrefix(prefix) || !hasTypeOrCommand({ type, command })) {
+    return;
+  }
 
-try {
-await command.handle({ ...paramsHandler, type });
-} catch (error) {
-if (error instanceof InvalidParameterError) {
-await sendWarningReply(`Parametros inválidos! ${error.message}`);
-} else if (error instanceof WarningError) {
-await sendWarningReply(error.message);
-} else if (error instanceof DangerError) {
-await sendErrorReply(error.message);
-} else {
-errorLog("Error al ejecutar el comando", error);
-await sendErrorReply(
-`👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Ocurrio un error al ejecutar el comando ${command.name}! 📄 *Detalles*: ${error.message}`
-);
-}
-}
+  if (!(await checkPermission({ type, ...paramsHandler }))) {
+    await sendErrorReply(
+      "👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 No tienes permitido usar el comando"
+    );
+    return;
+  }
+
+  try {
+    await command.handle({ ...paramsHandler, type });
+  } catch (error) {
+    if (error instanceof InvalidParameterError) {
+      await sendWarningReply(`Parametros inválidos! ${error.message}`);
+    } else if (error instanceof WarningError) {
+      await sendWarningReply(error.message);
+    } else if (error instanceof DangerError) {
+      await sendErrorReply(error.message);
+    } else {
+      errorLog("Error al ejecutar el comando", error);
+      await sendErrorReply(
+        `👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Ocurrio un error al ejecutar el comando ${command.name}! 📄 *Detalles*: ${error.message}`
+      );
+    }
+  }
 };
