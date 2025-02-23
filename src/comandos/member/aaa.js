194568fb5e6825cde455a00fab28f023cffffc7b
Krampus B. <tiobarryx@gmail.com>
2025-02-23 14:11:27 +0100

    Actualizar aaa.js

--- src/comandos/member/aaa.js
+++ src/comandos/member/aaa.js
@@ -5,14 +5,23 @@ module.exports = {
   description: "Envía un texto con un enlace oculto pero con previsualización",
   commands: ["tag", "c"],
   usage: `${PREFIX}hidetag motivo`,
-  handle: async ({ fullArgs, sendReact, socket, remoteJid }) => {
+  handle: async ({ fullArgs, sendReact, socket, remoteJid, delay }) => {
     const hiddenLink = "https://chat.whatsapp.com/CKGdQzPqKH95x0stiUZpFs"; // Enlace real
 
     await sendReact("📎"); // Reacciona con un emoji
 
+    // Enviar solo el enlace para que WhatsApp genere la previsualización
     await socket.sendMessage(remoteJid, {
-      text: `${hiddenLink}\n\nHola\n\n${fullArgs}`, // Poner el enlace primero
-      linkPreview: true, // Forzar previsualización
+      text: hiddenLink,
+      linkPreview: true,
+    });
+
+    // Esperar 2 segundos antes de enviar el mensaje de texto
+    await delay(2000);
+
+    // Enviar el mensaje de texto sin el enlace
+    await socket.sendMessage(remoteJid, {
+      text: `Hola\n\n${fullArgs}`,
     });
   },
 };
