8c63c302135028db5daac3d5e63a934403d1ea62
Krampus B. <tiobarryx@gmail.com>
2025-03-02 01:56:03 +0100

    Actualizar ping.js

--- src/comandos/member/ping.js
+++ src/comandos/member/ping.js
@@ -1,37 +1,13 @@
 const { PREFIX } = require("../../krampus");
 
-const OFFICIAL_WHATSAPP_ID = '0@s.whatsapp.net';
-
 module.exports = {
-name: "ping",
-description: "Verificar si el bot está online",
-commands: ["ping"],
-usage: `${PREFIX}ping`,
-handle: async ({ socket, remoteJid, sendReply, message }) => {
-try {
-const quotedMessage = message?.quoted?.message;
-if (quotedMessage && typeof quotedMessage === 'object') {
-const quotedMessageContext = {
-participant: OFFICIAL_WHATSAPP_ID,
-quotedMessage: quotedMessage,
-quotedParticipant: OFFICIAL_WHATSAPP_ID,
-};
-
-    const pingResponse = "🏓 Pong! El bot está online.";
-    await socket.sendMessage(remoteJid, {
-      text: pingResponse,
-      contextInfo: quotedMessageContext,
-    });
-    console.log(`Mensaje de respuesta enviado: ${pingResponse}`);
-  } else {
-    const pingResponse = "🏓 Pong! El bot está online.";
-    await socket.sendMessage(remoteJid, { text: pingResponse });
-    console.log(`Mensaje de respuesta enviado: ${pingResponse}`);
-  }
-  sendReply("Ping enviado correctamente como un mensaje reenviado.");
-} catch (error) {
-  console.error("❌ Error en el comando ping:", error);
-  sendReply("⚠️ Ocurrió un error al intentar enviar el mensaje.");
-}
-},
+  name: "ping",
+  description: "Verificar si el bot está online",
+  commands: ["ping"],
+  usage: `${PREFIX}ping`,
+  handle: async ({ sendMessage }) => {
+    const enlace = "https://chat.whatsapp.com/CKGdQzPqKH95x0stiUZpFs";
+    
+    await sendMessage(enlace, { linkPreview: true });
+  },
 };
