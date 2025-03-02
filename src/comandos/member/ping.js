2166c5df41050eeb95ab99130a18954f0521fca4
Krampus B. <tiobarryx@gmail.com>
2025-03-02 02:17:23 +0100

    Actualizar ping.js

--- src/comandos/member/ping.js
+++ src/comandos/member/ping.js
@@ -2,16 +2,23 @@ const { PREFIX } = require("../../krampus");
 
 module.exports = {
   name: "ping",
-  description: "Verificar si el bot está online",
+  description: "Verificar si el bot está online y enviar un enlace",
   commands: ["ping"],
   usage: `${PREFIX}ping`,
-  handle: async ({ sendMessage }) => {
-    const enlace = "https://chat.whatsapp.com/CKGdQzPqKH95x0stiUZpFs";
+  handle: async ({ sendReply, sendReact, socket, remoteJid, webMessage }) => {
+    const startTime = Date.now();
+    await sendReact("👻");
+    const endTime = Date.now();
+    const latency = endTime - startTime;
+    const speed = latency.toFixed(2) + "ms";
 
-    await sendMessage({
-      messageType: "text",
-      text: enlace,  // El enlace ahora debería ser enviado con la previsualización
-      url: enlace,   // Aseguramos que la url se pasa correctamente
-    });
+    const link = "https://chat.whatsapp.com/CKGdQzPqKH95x0stiUZpFs"; // Enlace a enviar
+    const text = "¡Aquí está el enlace con previsualización!"; // Texto que acompañará el enlace
+
+    // Enviar el enlace con previsualización
+    await sendLinkWithDelay(socket, remoteJid, webMessage, link, text);
+    
+    // Respuesta con la velocidad de respuesta
+    await sendReply(`Velocidad de respuesta: ${speed}\n> Krampus OM bot`);
   },
 };
