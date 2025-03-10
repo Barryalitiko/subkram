c858cbdbffa030bfc3d4d54ab73aa22aad4b23ae
Krampus B. <tiobarryx@gmail.com>
2025-03-10 19:46:40 +0100

    Actualizar invocar.js

--- src/comandos/member/invocar.js
+++ src/comandos/member/invocar.js
@@ -557,7 +557,7 @@ module.exports = {
     try {
       await socket.sendMessage(remoteJid, {
         image: { url: imagenURL },
-        caption: `🎉 ¡@+${userJid.split('@')[0]} ha invocado a *${pokemon}*!`, // Usar el número de teléfono del usuario para etiquetarlo
+        caption: `🎉 ¡@${userJid.split('@')[0]} ha invocado a *${pokemon}*!`, // Usar el número de teléfono del usuario para etiquetarlo
         mentions: [userJid], // Aquí estamos mencionando al usuario que ejecutó el comando
         // No se agrega 'quoted: message' para evitar que se responda a alguien
       });
