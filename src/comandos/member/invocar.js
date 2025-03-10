9d73bde6673a70bd5c63744e5e70de0deecdeaa0
Krampus B. <tiobarryx@gmail.com>
2025-03-10 19:50:46 +0100

    Actualizar invocar.js

--- src/comandos/member/invocar.js
+++ src/comandos/member/invocar.js
@@ -555,7 +555,7 @@ module.exports = {
 
     // Enviar la imagen correspondiente del Pokémon sin responder a nadie
     try {
-      await socket.sendMessage(userJid, {
+      await socket.sendMessage(remotedJid, {
         image: { url: imagenURL },
         caption: `🎉  @${userJid.split('@')[0]} ha invocado a *${pokemon}*`, // Usar el número de teléfono del usuario para etiquetarlo
         mentions: [userJid], // Aquí estamos mencionando al usuario que ejecutó el comando
