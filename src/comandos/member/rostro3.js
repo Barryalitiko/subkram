8e6c99e5578b6442bca9f1bafdbcdfcd74435b0f
Krampus B. <tiobarryx@gmail.com>
2025-03-19 13:39:57 +0100

    Actualizar rostro3.js

--- src/comandos/member/rostro3.js
+++ src/comandos/member/rostro3.js
@@ -58,7 +58,7 @@ module.exports = {
       let objetoImagen = await loadImage(ojosPath);
 
       // Coordenadas y dimensiones para los ojos (A)
-      const posicionX = 170; // Ajustar para que se alinee bien sobre la cara
+      const posicionX = 174; // Ajustar para que se alinee bien sobre la cara
       const posicionY = 250; // Ajustar para que se alinee bien sobre la cara, debajo de las gafas/lentes
       const ancho = 140;
       const alto = 40;
