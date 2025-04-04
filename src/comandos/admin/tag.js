0a8862c97931c4f9d1aa49b59076629a301d6bc0
Krampus B. <tiobarryx@gmail.com>
2025-04-04 17:34:38 +0200

    Update tag.js

--- src/comandos/admin/tag.js
+++ src/comandos/admin/tag.js
@@ -2,16 +2,34 @@ const { PREFIX } = require("../../krampus");
 
 module.exports = {
   name: "hide-tag",
-  description: "Para mencionar a todos",
+  description: "Para mencionar a todos y responder a mensajes",
   commands: ["tag", "t"],
   usage: `${PREFIX}hidetag motivo`,
-  handle: async ({ fullArgs, sendText, socket, remoteJid, sendReact }) => {
+  handle: async ({ fullArgs, sendText, sendImage, socket, remoteJid, sendReact, isReply, isImage, repliedMsg }) => {
     const { participants } = await socket.groupMetadata(remoteJid);
-
     const mentions = participants.map(({ id }) => id);
-
+    
     await sendReact("📎");
 
-    await sendText(`\n\n${fullArgs}`, mentions);
+    if (isReply) {
+      // Responde con el texto o imagen al mensaje original
+      if (isImage) {
+        const image = repliedMsg.imageUrl; // Obtén la URL de la imagen del mensaje respondido
+        const caption = fullArgs || repliedMsg.text; // Usa el texto después de tag o el texto original
+        await sendImage(image, caption, mentions);
+      } else {
+        // Si es solo texto, responde con el texto que sigue al comando
+        const messageText = fullArgs || repliedMsg.text;
+        await sendText(messageText, mentions);
+      }
+    } else {
+      // Si no es respuesta, pero tiene texto después del comando
+      if (fullArgs) {
+        await sendText(fullArgs, mentions);
+      } else {
+        // Si no hay texto después del comando, responde con el último mensaje
+        await sendText(repliedMsg.text, mentions);
+      }
+    }
   },
 };
