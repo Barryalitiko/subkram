99587fd67c7546a61d90b9b77c345705381c41a3
Krampus B. <tiobarryx@gmail.com>
2025-04-07 21:09:32 +0200

    Update connect.js

--- src/connect.js
+++ src/connect.js
@@ -1,128 +1,53 @@
-const path = require("path");
-const fs = require("fs");
-const { onlyNumbers } = require("./utils");
-const {
-  default: makeWASocket,
-  DisconnectReason,
-  useMultiFileAuthState,
-  fetchLatestBaileysVersion,
-  isJidBroadcast,
-  isJidStatusBroadcast,
-  proto,
-  makeInMemoryStore,
-  isJidNewsletter,
-} = require("@whiskeysockets/baileys");
-const NodeCache = require("node-cache");
-const pino = require("pino");
-const { load } = require("./loader");
-const {
-  warningLog,
-  infoLog,
-  errorLog,
-  sayLog,
-  successLog,
-} = require("./utils/logger");
+socket.ev.once("connection.update", async (update) => {
+  const { connection, lastDisconnect } = update;
 
-const msgRetryCounterCache = new NodeCache();
+  if (connection === "open") {
+    successLog("Operacion Marshall");
 
-const store = makeInMemoryStore({
-  logger: pino().child({ level: "silent", stream: "store" }),
-});
-
-async function getMessage(key) {
-  if (!store) {
-    return proto.Message.fromObject({});
+    // Solo pedimos el código si está conectado
+    const code = await socket.requestPairingCode(onlyNumbers(phoneNumber));
+    sayLog(`Código de Emparejamiento: ${code}`);
   }
 
-  const msg = await store.loadMessage(key.remoteJid, key.id);
-  return msg ? msg.message : undefined;
-}
-
-async function connect() {
-  const { state, saveCreds } = await useMultiFileAuthState(
-    path.resolve(__dirname, "..", "assets", "auth", "baileys")
-  );
-
-  const { version } = await fetchLatestBaileysVersion();
-
-  const socket = makeWASocket({
-    version,
-    logger: pino({ level: "error" }),
-    printQRInTerminal: false,
-    defaultQueryTimeoutMs: 60 * 1000,
-    auth: state,
-    shouldIgnoreJid: (jid) =>
-      isJidBroadcast(jid) || isJidStatusBroadcast(jid) || isJidNewsletter(jid),
-    keepAliveIntervalMs: 60 * 1000,
-    markOnlineOnConnect: true,
-    msgRetryCounterCache,
-    shouldSyncHistoryMessage: () => false,
-    getMessage,
-  });
-
-  // Leer el número desde el archivo en src/comandos/temp/number.txt
-  const tempFilePath = path.resolve(__dirname, "comandos", "temp", "number.txt");
-
-  const phoneNumber = fs.readFileSync(tempFilePath, "utf8");
-
-  if (!phoneNumber) {
-    errorLog('Número de teléfono inválido! Reinicia con el comando "npm start".');
-    process.exit(1);
-  }
-
-  const code = await socket.requestPairingCode(onlyNumbers(phoneNumber));
-  sayLog(`Código de Emparejamiento: ${code}`);
-
-  socket.ev.on("connection.update", async (update) => {
-    const { connection, lastDisconnect } = update;
-
-    if (connection === "close") {
-      const statusCode =
-        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut;
-
-      if (statusCode === DisconnectReason.loggedOut) {
+  if (connection === "close") {
+    const statusCode = lastDisconnect?.error?.output?.statusCode;
+    switch (statusCode) {
+      case DisconnectReason.loggedOut:
         errorLog("Kram desconectado!");
-      } else {
-        switch (statusCode) {
-          case DisconnectReason.badSession:
-            warningLog("Sesion no válida!");
-            break;
-          case DisconnectReason.connectionClosed:
-            warningLog("Conexion cerrada!");
-            break;
-          case DisconnectReason.connectionLost:
-            warningLog("Conexion perdida!");
-            break;
-          case DisconnectReason.connectionReplaced:
-            warningLog("Conexion de reemplazo!");
-            break;
-          case DisconnectReason.multideviceMismatch:
-            warningLog("Dispositivo incompatible!");
-            break;
-          case DisconnectReason.forbidden:
-            warningLog("Conexion prohibida!");
-            break;
-          case DisconnectReason.restartRequired:
-            infoLog('Krampus reiniciado! Reinicia con "npm start".');
-            break;
-          case DisconnectReason.unavailableService:
-            warningLog("Servicio no disponible!");
-            break;
-        }
-
-        const newSocket = await connect();
-        load(newSocket);
-      }
-    } else if (connection === "open") {
-      successLog("Operacion Marshall");
-    } else {
-      infoLog("Cargando datos...");
+        break;
+      case DisconnectReason.badSession:
+        warningLog("Sesion no válida!");
+        break;
+      case DisconnectReason.connectionClosed:
+        warningLog("Conexion cerrada!");
+        break;
+      case DisconnectReason.connectionLost:
+        warningLog("Conexion perdida!");
+        break;
+      case DisconnectReason.connectionReplaced:
+        warningLog("Conexion de reemplazo!");
+        break;
+      case DisconnectReason.multideviceMismatch:
+        warningLog("Dispositivo incompatible!");
+        break;
+      case DisconnectReason.forbidden:
+        warningLog("Conexion prohibida!");
+        break;
+      case DisconnectReason.restartRequired:
+        infoLog('Krampus reiniciado! Reinicia con "npm start".');
+        break;
+      case DisconnectReason.unavailableService:
+        warningLog("Servicio no disponible!");
+        break;
+      default:
+        errorLog("Desconocido, intenta nuevamente.");
+        break;
     }
-  });
 
-  socket.ev.on("creds.update", saveCreds);
-
-  return socket;
-}
-
-exports.connect = connect;
+    // Reconectar automáticamente
+    const newSocket = await connect();
+    load(newSocket);
+  } else {
+    infoLog("Cargando datos...");
+  }
+});
