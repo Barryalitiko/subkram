36d21eb0d7a3387cf4017f381bc3c77104ddd88d
Krampus B. <tiobarryx@gmail.com>
2025-04-09 21:59:50 +0200

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,26 +1,10 @@
 const { TIMEOUT_IN_MILLISECONDS_BY_EVENT } = require("./krampus");
 const { onMessagesUpsert } = require("./middlewares/onMessagesUpsert");
-const { onGroupParticipantsUpdate } = require("./middlewares/onGroupParticipantsUpdate");
+const {
+  onGroupParticipantsUpdate,
+} = require("./middlewares/onGroupParticipantsUpdate");
 
-exports.load = async (socket) => {
-  // Verifica si el socket es válido antes de intentar acceder a sus métodos
-  if (!socket || !socket.ev) {
-    console.error("Socket no está disponible o no tiene el evento 'ev'");
-    return;
-  }
-
-  // Espera hasta que el socket esté completamente conectado
-  if (!socket.isConnected()) {
-    console.log("Esperando a que el socket se conecte...");
-    await new Promise(resolve => {
-      socket.ev.on("connection.update", (update) => {
-        if (update.connection === "open") {
-          resolve();
-        }
-      });
-    });
-  }
-
+exports.load = (socket) => {
   socket.ev.on("messages.upsert", async ({ messages }) => {
     setTimeout(() => {
       onMessagesUpsert({ socket, messages });
