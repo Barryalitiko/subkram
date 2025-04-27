cc2a4eb84f4b1c3aa8653ee3ca863ec6dff432ae
Krampus B. <tiobarryx@gmail.com>
2025-04-27 15:40:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const { connect } = require("./connect");
 const { infoLog, bannerLog } = require("./utils/logger");
 
+let cloneCounter = 1; // Empezamos en 1 para identificar cada subbot
+
 async function start() {
   try {
     bannerLog();
     infoLog("Kram está procesando...");
 
     while (true) {
-      await connect();
-      await new Promise((resolve) => setTimeout(resolve, 5000)); // Espera 5 segundos antes de volver a intentar si falla
+      await connect(cloneCounter); // Le pasamos el número de clon
+      cloneCounter++; // Incrementamos para el siguiente subbot
+      await new Promise((resolve) => setTimeout(resolve, 5000)); // Espera 5 segundos antes de volver a intentar
     }
 
   } catch (error) {
